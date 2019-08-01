a10dbb6a516466f38242ee5a41af9f16b376aa74
GTMLDee <liudi0304@163.com>
2019-08-01 09:59:10 +0800

    modify comment

--- src/main/java/summer/jaffer/spring/aop/LogAspect.java
+++ src/main/java/summer/jaffer/spring/aop/LogAspect.java
@@ -16,6 +16,7 @@ import java.lang.reflect.Parameter;
  * 实现打印所有加@Log注解的方法的参数的打印
  * 切点、通知、连接点、切面、织入概念见序号1-5
  * 5.整个AOP代理对象加载到内存的过程为"织入"
+ * Spring AOP中被代理的A方法内部直接调用同类的B方法，则B方法即使加@Log注解也不会被切面代理，因为调用B时未调用代理方法
  */
 @Component
 @Aspect // 4.定义切面
