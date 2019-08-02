1e4be17f7369c9f7a872c14302957e8638f166ec
liud <liudi@seentao.com>
2019-08-02 08:41:57 +0800

    add select sort

--- src/main/java/summer/jaffer/datastructor/sort/Sort8Algorithm.java
+++ src/main/java/summer/jaffer/datastructor/sort/Sort8Algorithm.java
@@ -52,4 +52,26 @@ public class Sort8Algorithm {
         }
         return arr;
     }
+
+    /**
+     *  选择排序
+     *  1.直接选择排序
+     * @param arr
+     * @return
+     */
+    public static int[] selectSort(int[] arr) {
+        if (arr == null || arr.length == 0) return arr;
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+        return arr;
+    }
 }
