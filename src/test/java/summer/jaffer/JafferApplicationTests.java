71fd072cf6bdd552d793c58337961ffca21d62d0
liud <liudi@seentao.com>
2019-08-13 01:51:16 +0800

    add comment

--- src/test/java/summer/jaffer/JafferApplicationTests.java
+++ src/test/java/summer/jaffer/JafferApplicationTests.java
@@ -1,16 +1,16 @@
-package summer.jaffer;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class JafferApplicationTests {
-
-    @Test
-    public void contextLoads() {
-    }
-
-}
+//package summer.jaffer;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class JafferApplicationTests {
+//
+//    @Test
+//    public void contextLoads() {
+//    }
+//
+//}
